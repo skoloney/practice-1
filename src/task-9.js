04ee8c5509ea579551c8132436e492e19e6f5c50
Krapolon <Kablyk1997@gmail.com>
2019-07-22 19:02:53 +0300

    All 1 practice

--- src/task-9.js
+++ src/task-9.js
@@ -5,12 +5,10 @@ export default function merge(data) {
         for (const b in i) {
             if (!obj[b]) {
                 obj[b] = [i[b]];
-                console.log("b", b);
             } else {
                 obj[b].push(i[b]);
             }
         }
-        console.log("obj", obj);
     }
     return obj;
 }
